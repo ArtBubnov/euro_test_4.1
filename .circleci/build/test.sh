3a64d0e7912ceea744e10d6d800aa77b60b19e85
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-01-04 16:13:35 +0300

    trest

--- .circleci/build/test.sh
+++ .circleci/build/test.sh
@@ -1,17 +1,16 @@
- TEST="test"
- if [[ -z ${"TEST"} ]] ; then 
-    echo "PASS"
+if [[ -z ${CIRCLE_PULL_REQUEST} ]] ; then 
+        echo "PASS"
     else
-    echo "ERROR"
-
+        echo "ERROR"
     fi
 
 
 
- #if [[ -z ${CIRCLE_PULL_REQUEST} ]] ; then 
 
+# TEST="test"
+# if [[ -z ${"TEST"} ]] ; then 
+#    echo "PASS"
 #    else
-#        PULL_REQUEST_ID=${CIRCLE_PULL_REQUEST##*/}
+#    echo "ERROR"
 
-#
 #    fi
